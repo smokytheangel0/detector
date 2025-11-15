43d628d0dac9f3516b70566889a5630523fef8b8
smokytheangel0 <j.m.a.kierstead@gmail.com>
2025-11-14 23:33:59 -0800

    missed a bit

--- src/main.rs
+++ src/main.rs
@@ -21,4 +21,25 @@ fn main() {
                 Pluto.run(notebook = joinpath(pwd(), \"whitepaper.jl\"))")
         }.expect("an exception occurred");
     });
+    //parse verus using vest with dsl translated from the verus.pest file.
+        //functions linked with struct and sother impls if method or associated function
+            //exec  sig + body
+            //spec  sig + body
+            //proof sig + body
+        //structs
+        //enums
+        //primatives
+    //generate pluto controls using jlrs
+        //snippet box controls
+            //|prettified exec only
+                //this requires generating translations for code using vstd idioms
+            //|a function its the proofs, specs, linked with the other functions, proofs and specs that it depends on
+            //|auto matching highlighted assembly ()
+                //0 => new browser window (compiler-explorer)
+                //1..n => new tab in (compiler-explorer)
+
+
+        
+        
+
 }
